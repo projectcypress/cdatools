40ec8881508e6f734013fd2733cd62e6b27416a0
Andrew Hubley <ahubley@mitre.org>
2016-01-20 14:00:14 -0500

    added test suite for import of demographics, encounters, and diagnoses

--- importer/importer_test.go
+++ importer/importer_test.go
@@ -62,7 +62,36 @@ func (i *ImporterSuite) TestExtractEncounters(c *C) {
 	c.Assert(encounter.EndTime, Equals, 1288569600)
 }
 
-/*
 func (i *ImporterSuite) TestExtractDiagnoses(c *C) {
+	var diagnosisXPath = xpath.Compile("//cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.11']")
+	rawDiagnoses := ExtractSection(i.patientElement, diagnosisXPath, DiagnosisExtractor, "2.16.840.1.113883.3.560.1.2")
+	i.patient.Diagnoses = make([]models.Diagnosis, len(rawDiagnoses))
+	for j := range rawDiagnoses {
+		i.patient.Diagnoses[j] = rawDiagnoses[j].(models.Diagnosis)
+	}
 
-}*/
+	c.Assert(len(i.patient.Diagnoses), Equals, 3)
+	firstDiagnosis := i.patient.Diagnoses[0]
+	c.Assert(firstDiagnosis.ID.Root, Equals, "1.3.6.1.4.1.115")
+	c.Assert(firstDiagnosis.ID.Extension, Equals, "54c1142869702d2cd2520100")
+	c.Assert(firstDiagnosis.Codes["SNOMED-CT"], Equals, "195080001")
+	c.Assert(firstDiagnosis.Description, Equals, "Diagnosis, Active: Atrial Fibrillation/Flutter")
+	c.Assert(firstDiagnosis.StartTime, Equals, 1332720000)
+	c.Assert(firstDiagnosis.EndTime, Equals, 0)
+
+	secondDiagnosis := i.patient.Diagnoses[1]
+	c.Assert(secondDiagnosis.ID.Root, Equals, "1.3.6.1.4.1.115")
+	c.Assert(secondDiagnosis.ID.Extension, Equals, "54c1142969702d2cd2cd0200")
+	c.Assert(secondDiagnosis.Codes["SNOMED-CT"], Equals, "237244005")
+	c.Assert(secondDiagnosis.Description, Equals, "Diagnosis, Active: Pregnancy Dx")
+	c.Assert(secondDiagnosis.StartTime, Equals, 1362096000)
+	c.Assert(secondDiagnosis.EndTime, Equals, 1382227200)
+
+	thirdDiagnosis := i.patient.Diagnoses[2]
+	c.Assert(thirdDiagnosis.ID.Root, Equals, "1.3.6.1.4.1.115")
+	c.Assert(thirdDiagnosis.ID.Extension, Equals, "54c1142869702d2cd2760100")
+	c.Assert(thirdDiagnosis.Codes["SNOMED-CT"], Equals, "46635009")
+	c.Assert(thirdDiagnosis.Description, Equals, "Diagnosis, Active: Diabetes")
+	c.Assert(thirdDiagnosis.StartTime, Equals, 1361836800)
+	c.Assert(thirdDiagnosis.EndTime, Equals, 0)
+}
