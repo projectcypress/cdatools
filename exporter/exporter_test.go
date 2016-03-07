581525af8402d274225cbfa6e1663acf226bf61c
Andrew Schreiber <aschreiber@mitre.org>
2016-03-07 14:00:18 -0500

    Change test to load from fixtures.

--- exporter/exporter_test.go
+++ exporter/exporter_test.go
@@ -3,11 +3,20 @@ package exporter
 import (
 	"fmt"
 	"testing"
+	"github.com/pebbe/util"
+	"io/ioutil"
 )
 
 // This test is essentially a noop but it's useful to see what you're exporting.
 // More functional tests are in the go-cda-repo where we run the exports through
 //  HDS validation.
 func TestExport(t *testing.T) {
-	fmt.Println(GenerateCat1([]byte("Foo"), []byte("")))
+	patientData, err := ioutil.ReadFile("../fixtures/records/barry_berry.json")
+	util.CheckErr(err)
+
+	measureData, err := ioutil.ReadFile("../fixtures/measures/CMS9v4a.json")
+	util.CheckErr(err)
+	fmt.Println(string(measureData))
+
+	fmt.Println(GenerateCat1(patientData, measureData))
 }
