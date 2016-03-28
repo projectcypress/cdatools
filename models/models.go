d47be7de81cd2b7f556f05ca59c4d6fc9bd07f4c
Michael O'Keefe <mokeefe@mitre.org>
2016-03-28 11:08:35 -0400

    Added insurance provider model and some instance methods

--- models/models.go
+++ models/models.go
@@ -84,11 +84,50 @@ type Record struct {
 	Person
 	MedicalRecordNumber   string                `json:"medical_record_number"`
 	MedicalRecordAssigner string                `json:"medical_record_assigner"`
+	BirthDate             int64                 `json:"birthdate"`
+	DeathDate             int64                 `json:"deathdate"`
+	Expired               bool                  `json:"expired"`
 	Encounters            []Encounter           `json:"encounters"`
 	Diagnoses             []Diagnosis           `json:"conditions"`
 	LabResults            []LabResult           `json:"results"`
 	Languages             []Language            `json:"languages"`
 	ProviderPerformances  []ProviderPerformance `json:"provider_performances"`
+	InsuranceProviders    []InsuranceProvider   `json:"insurance_providers"`
+}
+
+// Entries returns all the entries from the Encounters, Diagnoses, and LabResults for a Record
+func (r *Record) Entries() []interface{} {
+	var entries []interface{}
+
+	// This whole "for loop for each of these things" is unavoidable, because elements must be copied individually to a []interface{}
+	for _, en := range r.Encounters {
+		entries = append(entries, en)
+	}
+
+	for _, di := range r.Diagnoses {
+		entries = append(entries, di)
+	}
+
+	for _, lr := range r.LabResults {
+		entries = append(entries, lr)
+	}
+
+	for _, ip := range r.InsuranceProviders {
+		entries = append(entries, ip)
+	}
+
+	return entries
+}
+
+// EntriesForOid returns all the entries which include the OID
+func (r *Record) EntriesForOid(oid string) []interface{} {
+	var matchedEntries []interface{}
+	for _, entry := range r.Entries() {
+		if entry.(Entry).Oid == oid {
+			matchedEntries = append(matchedEntries, entry)
+		}
+	}
+	return matchedEntries
 }
 
 type ResultValue struct {
