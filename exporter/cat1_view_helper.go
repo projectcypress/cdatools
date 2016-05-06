6b0eee09b112c7344b1642c953f7928e6b453f22
Rob Dingwell <bobd@mitre.org>
2016-05-06 13:08:28 -0400

    initial implementation of codeDispaly and valueOrNullFlavor template utility methods

--- exporter/cat1_view_helper.go
+++ exporter/cat1_view_helper.go
@@ -5,9 +5,12 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/xml"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/projectcypress/cdatools/models"
 )
 
 func escape(i interface{}) string {
@@ -53,4 +56,99 @@ func identifierForInt(objs ...int64) string {
 func identifierForString(objs ...string) string {
 	b := strings.Join(objs, ",")
 	return identifierFor([]byte(b))
+}
+
+func valueOrNullFlavor(i interface{}) string {
+	var s string
+	switch str := i.(type) {
+	case string:
+		ival, err := strconv.Atoi(str)
+		if err != nil {
+			var t = time.Unix(int64(ival), 0)
+			s = fmt.Sprintf("value='%s'", t)
+		}
+	case int64:
+		var t = time.Unix(str, 0)
+		s = fmt.Sprintf("value='%s'", t)
+	case int:
+		var t = time.Unix(int64(str), 0)
+		s = fmt.Sprintf("value='%s'", t)
+	default:
+		s = "nullFlavor='UNK'"
+	}
+	return s
+}
+
+func valueOrDefault(val interface{}, def interface{}) interface{} {
+	if val != nil {
+		return val
+	}
+	return def
+}
+
+func codeDisplay(entry models.Entry, options map[string]interface{}) string {
+	tagName := valueOrDefault(options["tag_name"], "code")
+	attribute := valueOrDefault(options["attribute"], "codes")
+	excludeNullFlavor := valueOrDefault(options["exclude_null_flavor"], false)
+	extraContent := valueOrDefault(options["extra_content"], "")
+	var codeString string
+	var pcs []string
+	if options["preferred_code_sets"] == "*" {
+		pcs = models.CodeSystemNames()
+	} else {
+		pcs = options["preferred_code_sets"].([]string)
+	}
+
+	if pcs != nil {
+	}
+	// need to replace this with actual call to entry.perferredCode once implmented
+	preferredCode := map[string]string{"code": "1234",
+		"code_set": "SNOMED-CT"}
+	if preferredCode != nil {
+		oid := models.OidForCodeSystem(preferredCode["code_set"])
+		codeString = fmt.Sprintf("<%s code='%s' codeSystem='%s' %s>", tagName, preferredCode["code"], oid, extraContent)
+	} else {
+		var buffer bytes.Buffer
+		buffer.WriteString(fmt.Sprintf("<%s ", tagName))
+		if !excludeNullFlavor.(bool) {
+			buffer.WriteString(" nullFlavor='UNK' ")
+		}
+		buffer.WriteString(extraContent.(string))
+		codeString = buffer.String()
+	}
+	if attribute == "codes" {
+		codeString += fmt.Sprintf("<originalText>%s</originalText>", entry.Description)
+		// add a bunch of translation codes if they exist
+	}
+	// pcs = if options['preferred_code_sets'] && options['preferred_code_sets'].index("*")
+	// 			# all of the code_systems that we know about
+	// 			HealthDataStandards::Util::CodeSystemHelper::CODE_SYSTEMS.values | HealthDataStandards::Util::CodeSystemHelper::CODE_SYSTEM_ALIASES.keys
+	// 		else
+	// 			options['preferred_code_sets']
+	// 		end
+	// preferred_code = entry.preferred_code(pcs, options['attribute'], options['value_set_map'])
+	//       if preferred_code
+	//         code_system_oid = HealthDataStandards::Util::CodeSystemHelper.oid_for_code_system(preferred_code['code_set'])
+	//         code_string = "<#{options['tag_name']} code=\"#{preferred_code['code']}\" codeSystem=\"#{code_system_oid}\" #{options['extra_content']}>"
+	//       else
+	//         code_string = "<#{options['tag_name']} "
+	//         code_string += "nullFlavor=\"UNK\" " unless options["exclude_null_flavor"]
+	//         code_string += "#{options['extra_content']}>"
+	//       end
+	//
+	//
+	//
+	//       if options["attribute"] == :codes && entry.respond_to?(:translation_codes)
+	//         code_string += "<originalText>#{ERB::Util.html_escape entry.description}</originalText>" if entry.respond_to?(:description)
+	//         entry.translation_codes(options['preferred_code_sets'], options['value_set_map']).each do |translation|
+	//           code_string += "<translation code=\"#{translation['code']}\" codeSystem=\"#{HealthDataStandards::Util::CodeSystemHelper.oid_for_code_system(translation['code_set'])}\"/>\n"
+	//         end
+	//       end
+	//
+	//       code_string += "</#{options['tag_name']}>"
+	//
+	//       code_string
+	//     end
+
+	return fmt.Sprintf("%s </%s>", codeString, tagName)
 }
--- models/code_system_map.go
+++ models/code_system_map.go
@@ -1,19 +1,84 @@
 package models
 
 var oidMap = map[string]string{
-	"2.16.840.1.113883.6.12":     "CPT",
-	"2.16.840.1.113883.6.1":      "LOINC",
-	"2.16.840.1.113883.6.96":     "SNOMED-CT",
-	"2.16.840.1.113883.6.88":     "RxNorm",
-	"2.16.840.1.113883.6.103":    "ICD-9-CM",
-	"2.16.840.1.113883.6.104":    "ICD-9-PCS",
-	"2.16.840.1.113883.6.4":      "ICD-10-PCS",
-	"2.16.840.1.113883.6.90":     "ICD-10-CM",
-	"2.16.840.1.113883.3.221.5":  "SOP",
-	"2.16.840.1.113883.3.26.1.1": "NCI Thesaurus",
-	"2.16.840.1.113883.5.4":      "ActCode",
-}
+	"2.16.840.1.113883.6.1":           "LOINC",
+	"2.16.840.1.113883.6.96":          "SNOMED-CT",
+	"2.16.840.1.113883.6.12":          "CPT",
+	"2.16.840.1.113883.6.88":          "RxNorm",
+	"2.16.840.1.113883.6.103":         "ICD-9-CM",
+	"2.16.840.1.113883.6.104":         "ICD-9-PCS",
+	"2.16.840.1.113883.6.4":           "ICD-10-PCS",
+	"2.16.840.1.113883.6.90":          "ICD-10-CM",
+	"2.16.840.1.113883.6.14":          "HCP",
+	"2.16.840.1.113883.6.285":         "HCPCS",
+	"2.16.840.1.113883.5.2":           "HL7 Marital Status",
+	"2.16.840.1.113883.12.292":        "CVX",
+	"2.16.840.1.113883.5.83":          "HITSP C80 Observation Status",
+	"2.16.840.1.113883.3.26.1.1":      "NCI Thesaurus",
+	"2.16.840.1.113883.3.88.12.80.20": "FDA",
+	"2.16.840.1.113883.4.9":           "UNII",
+	"2.16.840.1.113883.6.69":          "NDC",
+	"2.16.840.1.113883.5.14":          "HL7 ActStatus",
+	"2.16.840.1.113883.6.259":         "HL7 Healthcare Service Location",
+	"2.16.840.1.113883.12.112":        "DischargeDisposition",
+	"2.16.840.1.113883.5.4":           "HL7 Act Code",
+	"2.16.840.1.113883.1.11.18877":    "HL7 Relationship Code",
+	"2.16.840.1.113883.6.238":         "CDC Race",
+	"2.16.840.1.113883.6.177":         "NLM MeSH",
+	"2.16.840.1.113883.5.1076":        "Religious Affiliation",
+	"2.16.840.1.113883.1.11.19717":    "HL7 ActNoImmunicationReason",
+	"2.16.840.1.113883.3.88.12.80.33": "NUBC",
+	"2.16.840.1.113883.1.11.78":       "HL7 Observation Interpretation",
+	"2.16.840.1.113883.3.221.5":       "Source of Payment Typology",
+	"2.16.840.1.113883.6.13":          "CDT",
+	"2.16.840.1.113883.18.2":          "AdministrativeSex"}
+
+var codeSystemAliases = map[string]string{
+	"FDA SPL": "NCI Thesaurus",
+	"HSLOC":   "HL7 Healthcare Service Location",
+	"SOP":     "Source of Payment Typology"}
+
+var oidAliases = map[string]string{
+	"2.16.840.1.113883.6.59": "2.16.840.1.113883.12.292"}
 
 func CodeSystemFor(oid string) string {
-	return oidMap[oid]
+	var oidString = oid
+	var alias, ok = oidAliases[oid]
+	if ok {
+		oidString = alias
+	}
+	return oidMap[oidString]
+}
+
+func invertMap(toInvert map[string]string) map[string]string {
+	var inverted = map[string]string{}
+	for k, v := range toInvert {
+		inverted[v] = k
+	}
+	return inverted
+}
+
+func OidForCodeSystem(codeSystem string) string {
+	var cs = codeSystem
+	var alias, ok = codeSystemAliases[codeSystem]
+	if ok {
+		cs = alias
+	}
+	var inverted = invertMap(oidMap)
+	return inverted[cs]
+}
+
+func CodeSystemNames() []string {
+	keys := make([]string, 0, len(oidMap)+len(codeSystemAliases))
+	for _, v := range oidMap {
+		keys = append(keys, v)
+	}
+	for _, v := range codeSystemAliases {
+		keys = append(keys, v)
+	}
+	return keys
+}
+
+func CodeSystems() map[string]string {
+	return oidMap
 }
