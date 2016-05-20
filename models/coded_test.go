4f52ae2bc7033e8b84ec03f5ba4a1efe8dc464de
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-05-20 11:14:57 -0400

    Moved preferred code test to entry_test.go

--- models/coded_test.go
+++ models/coded_test.go
@@ -13,15 +13,3 @@ func TestIntersection(t *testing.T) {
 		t.Error("Incorrect number of intersecting elements")
 	}
 }
-
-func TestPreferredCode(t *testing.T) {
-	codes := make(map[string][]string, 2)
-	codes["a"] = []string{"aa", "ab"}
-	codes["b"] = []string{"ba", "bb"}
-	coded := Coded{Codes: codes}
-	entry := Entry{Coded: coded}
-	prefCode := entry.PreferredCode([]string{"b"})
-	if prefCode.Code != "ba" {
-		t.Error("Returned incorrect code, expected", "ba", "got", prefCode.Code)
-	}
-}
