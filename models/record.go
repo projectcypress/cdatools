2248bcffdb2aee321de19b35ca386f350a5c2b37
Matt <mattrbianchi@gmail.com>
2016-11-04 23:55:54 -0400

    Refactor ResolveReference and more cases in EntriesForDataCriteria
    ResolveReference was iterating over all the entries in record
    when it could immediately resolve which group of entries it
    needed to search through. This refactoring then removed the need
    for EntriesMap.
    Also in this commit is some shortening of the
    logic in the second switch statement in EntriesForDataCriteria.
    This allows the code to be reasoned about more easily.

--- models/record.go
+++ models/record.go
@@ -2,43 +2,60 @@ package models
 
 import (
 	"regexp"
-
-	"github.com/pborman/uuid"
 )
 
 type Record struct {
 	Person
+	RecordGroup
 	MedicalRecordNumber   string                `json:"medical_record_number,omitempty"`
 	MedicalRecordAssigner string                `json:"medical_record_assigner,omitempty"`
 	BirthDate             int64                 `json:"birthdate,omitempty"`
 	DeathDate             int64                 `json:"deathdate,omitempty"`
 	Expired               bool                  `json:"expired,omitempty"`
-	Encounters            []Encounter           `json:"encounters,omitempty"`
-	LabResults            []LabResult           `json:"results,omitempty"`
-	Languages             []Language            `json:"languages,omitempty"`
-	ProviderPerformances  []ProviderPerformance `json:"provider_performances,omitempty"`
-	InsuranceProviders    []InsuranceProvider   `json:"insurance_providers,omitempty"`
-	Procedures            []Procedure           `json:"procedures,omitempty"`
-	Medications           []Medication          `json:"medications, omitempty"`
-	Allergies             []Allergy             `json:"allergies,omitempty"`
-	Conditions            []Condition           `json:"conditions,omitempty"`
-	Communications        []Communication       `json:"communications,omitempty"`
-	MedicalEquipment      []MedicalEquipment    `json:"medical_equipment,omitempty"`
-	CareGoals             []Entry               `json:"care_goals,omitempty"`
+	// Private values to handle querying of Entries
+	
+}
+
+
+type RecordGroup struct {
+	Encounters            EncounterGroup           `json:"encounters,omitempty"`
+	LabResults            LabResultsGroup           `json:"results,omitempty"`
+	ProviderPerformances  ProviderPerformanceGroup `json:"provider_performances,omitempty"`
+	InsuranceProviders    InsuranceProviderGroup   `json:"insurance_providers,omitempty"`
+	Procedures            ProcedureGroup           `json:"procedures,omitempty"`
+	Medications           MedicationGroup          `json:"medications, omitempty"`
+	Allergies             AllergyGroup             `json:"allergies,omitempty"`
+	Conditions            ConditionGroup           `json:"conditions,omitempty"`
+	
+	// These weren't in the Entries() method.
+	Languages             LanguagesGroup            `json:"languages,omitempty"`
+	Communications        CommunicationGroup       `json:"communications,omitempty"`
+	MedicalEquipment      MedicalEquipmentGroup    `json:"medical_equipment,omitempty"`
+	CareGoals             EntryGroup               `json:"care_goals,omitempty"`
 }
 
 type Language struct {
 	Coded
 }
 
-//type EntryGroup []Entry
-//type EncounterGroup []Encounter
-//type LabResults []LabResult
+type EntryGroup []Entry
+type EncounterGroup []Encounter
+type LabResultsGroup []LabResult
+type LanguagesGroup []Language
+type ProviderPerformanceGroup []ProviderPerformance
+type InsuranceProviderGroup []InsuranceProvider
+type ProcedureGroup []Procedure
+type MedicationGroup []Medication
+type AllergyGroup []Allergy
+type ConditionGroup []Condition
+type CommunicationGroup []Communication
+type MedicalEquipmentGroup []MedicalEquipment
 
-//type EntryService interface {
-//	EntriesForDataCriteria(DataCriteria) []EntryGroup
-//	EntriesForOid(oid string) []EntryGroup
-//}
+
+type EntryService interface {
+	EntriesForDataCriteria(DataCriteria) EntryGroup
+	EntriesForOid(oid string) EntryGroup
+}
 
 // Entries returns all the entries from the Encounters, Diagnoses, and LabResults for a Record
 func (r *Record) Entries() []HasEntry {
@@ -80,45 +97,6 @@ func (r *Record) Entries() []HasEntry {
 	return entries
 }
 
-// Entries returns all the entries from the Encounters, Diagnoses, and LabResults for a Record
-func (r *Record) EntryMap() map[string][]HasEntry {
-	var entries map[string][]HasEntry
-
-	// This whole "for loop for each of these things" is unavoidable, because elements must be copied individually to a []HasEntry
-	for i := range r.Encounters {
-		entries["Encounters"] = append(entries["Encounters"], &r.Encounters[i])
-	}
-
-	for i := range r.LabResults {
-		entries["LabResults"] = append(entries["LabResults"], &r.LabResults[i])
-	}
-
-	for i := range r.InsuranceProviders {
-		entries["InsuranceProviders"] = append(entries["InsuranceProviders"], &r.InsuranceProviders[i])
-	}
-
-	for i := range r.ProviderPerformances {
-		entries["ProviderPerformances"] = append(entries["ProviderPerformances"], &r.ProviderPerformances[i])
-	}
-
-	for i := range r.Procedures {
-		entries["Procedures"] = append(entries["Procedures"], &r.Procedures[i])
-	}
-
-	for i := range r.Medications {
-		entries["Medications"] = append(entries["Medications"], &r.Medications[i])
-	}
-
-	for i := range r.Allergies {
-		entries["Allergies"] = append(entries["Allergies"], &r.Allergies[i])
-	}
-
-	for i := range r.Conditions {
-		entries["Conditions"] = append(entries["Conditions"], &r.Conditions[i])
-	}
-
-	return entries
-}
 
 // EntriesForOid returns all the entries which include the OID
 func (r *Record) EntriesForOid(oid string) []HasEntry {
@@ -133,30 +111,22 @@ func (r *Record) EntriesForOid(oid string) []HasEntry {
 
 // GetEntriesForOids returns all the entries which include the list of OIDs given
 func (r *Record) GetEntriesForOids(oids ...string) []HasEntry {
-	var matchedEntries []HasEntry
+	var entries []HasEntry
 	for _, entry := range r.Entries() {
 		for _, oid := range oids {
 			if entry.GetEntry().Oid == oid {
-				matchedEntries = append(matchedEntries, entry)
+				entries = append(entries, entry)
 			}
 		}
 	}
-	return matchedEntries
+	return entries
 }
 
-// ResolveReference takes a Reference object, and finds the Entry that it refers to
-func (r *Record) ResolveReference(ref Reference) HasEntry {
-	for _, entry := range r.EntryMap()[ref.ReferencedType] {
-		if entry.GetEntry().ID.Extension == ref.ReferencedID {
-			return entry
-		}
-	}
-	return nil
-}
+
 
 func (r *Record) EntriesForDataCriteria(dataCriteria DataCriteria, vsMap map[string][]CodeSet) []HasEntry {
 	dataCriteriaOid := dataCriteria.HQMFOid
-
+	
 	var entries []HasEntry
 	var filteredEntries []HasEntry
 	switch dataCriteriaOid {
@@ -167,6 +137,7 @@ func (r *Record) EntriesForDataCriteria(dataCriteria DataCriteria, vsMap map[str
 	default:
 
 		var codes []CodeSet
+		codes = vsMap[dataCriteria.CodeListID]
 		switch dataCriteriaOid {
 		case "2.16.840.1.113883.3.560.1.5", "2.16.840.1.113883.3.560.1.12":
 			// If Lab Test: Performed, look for Lab Test: Result too
@@ -177,65 +148,70 @@ func (r *Record) EntriesForDataCriteria(dataCriteria DataCriteria, vsMap map[str
 			entries = r.GetEntriesForOids("2.16.840.1.113883.3.560.1.3", "2.16.840.1.113883.3.560.1.11")
 		case "2.16.840.1.113883.3.560.1.71", "2.16.840.1.113883.3.560.1.72":
 			// Transfers (either from or to)
-			entries = r.GetEntriesForOids(dataCriteriaOid, "2.16.840.1.113883.3.560.1.79")
 			if dataCriteria.FieldValues != nil {
-				codeListID := dataCriteria.FieldValues["TRANSFER_FROM"].CodeListID
-				if codeListID == "" {
-					codeListID = dataCriteria.FieldValues["TRANSFER_TO"].CodeListID
-					codes = vsMap[codeListID]
+				if dataCriteria.FieldValues["TRANSFER_FROM"].CodeListID == "" {
+					codes = vsMap[dataCriteria.FieldValues["TRANSFER_TO"].CodeListID]
 				}
 			}
+			entries = r.GetEntriesForOids(dataCriteriaOid, "2.16.840.1.113883.3.560.1.79")
 		default:
 			entries = r.GetEntriesForOids(dataCriteriaOid)
 		}
+		
+// Gonna have to do for now.
+		// Get a slice containing only unique entries
+//		ids := make(map[string]struct{})
+//		uniqueEntries := make([]HasEntry, len(entries))
+//		for _, entry := range entries {
+//			if _, ok := ids[entry.GetEntry().BSONID]; ok {
+//				continue
+//			}
+//			uniqueEntries = append(uniqueEntries, entry)
+//			ids[entry.GetEntry().BSONID] = *new(struct{})
+//		}
 
-		if codes == nil {
-			codes = vsMap[dataCriteria.CodeListID]
+
+		filteredEntries = r.handleCodeSetsAndTransfers(entries, filteredEntries, dataCriteria, codes)
 		}
 
-		// Get a slice containing only unique entries, by adding them to a map, then iterating over that
-		// NOTE: this makes me hate myself
-		uniqueEntries := make(map[string]HasEntry)
-		for _, entry := range entries {
-			uniqueEntries[string(uuid.NewRandom())] = entry
-		}
-		var negationRegexp = regexp.MustCompile(`2\.16\.840\.1\.113883\.3\.526\.3\.100[7-9]`)
-		for _, entry := range uniqueEntries {
+	return filteredEntries
+}
 
-			entryData := entry.GetEntry()
-
-			if negationRegexp.FindStringIndex(dataCriteria.CodeListID) != nil {
-				// Add the entry to FilteredEntries if entry.negation_reason['code'] is in codes
-				if reasonInCodes(codes[0], entryData.NegationReason) {
-					filteredEntries = append(filteredEntries, entry)
-				}
-			} else if dataCriteriaOid == "2.16.840.1.113883.3.560.1.71" {
-				if transferFrom := &entry.(*Encounter).TransferFrom; transferFrom != nil {
-					transferFrom.Codes[transferFrom.CodeSystem] = []string{transferFrom.Code}
-					tfc := transferFrom.Coded.CodesInCodeSet(codes[0].Set)
-					if len(tfc) > 0 {
-						filteredEntries = append(filteredEntries, entry)
-					}
-				}
-			} else if dataCriteriaOid == "2.16.840.1.113883.3.560.1.72" {
-				if transferTo := &entry.(*Encounter).TransferTo; transferTo != nil {
-					transferTo.Codes[transferTo.CodeSystem] = []string{transferTo.Code}
-					if len(transferTo.Coded.CodesInCodeSet(codes[0].Set)) > 0 {
-						filteredEntries = append(filteredEntries, entry)
-					}
-				}
-			} else {
-				if entryData.IsInCodeSet(codes) && entryData.NegationInd != nil {
-					if *entryData.NegationInd == dataCriteria.Negation {
-						filteredEntries = append(filteredEntries, entry)
-					}
-				} else if entryData.IsInCodeSet(codes) && entryData.NegationInd == nil && !dataCriteria.Negation {
+func (r *Record) handleCodeSetsAndTransfers(entries []HasEntry, filteredEntries []HasEntry, dataCriteria DataCriteria, codes []CodeSet) []HasEntry {
+	dataCriteriaOid := dataCriteria.HQMFOid
+	negationRegexp := regexp.MustCompile(`2\.16\.840\.1\.113883\.3\.526\.3\.100[7-9]`)
+	for _, entry := range entries {
+		entryData := entry.GetEntry()
+		if negationRegexp.FindStringIndex(dataCriteria.CodeListID) != nil {
+			// Add the entry to FilteredEntries if entry.negation_reason['code'] is in codes
+			if reasonInCodes(codes[0], entryData.NegationReason) {
+				filteredEntries = append(filteredEntries, entry)
+			}
+		} else if dataCriteriaOid == "2.16.840.1.113883.3.560.1.71" {
+			if transferFrom := &entry.(*Encounter).TransferFrom; transferFrom != nil {
+				transferFrom.Codes[transferFrom.CodeSystem] = []string{transferFrom.Code}
+				tfc := transferFrom.Coded.CodesInCodeSet(codes[0].Set)
+				if len(tfc) > 0 {
 					filteredEntries = append(filteredEntries, entry)
 				}
 			}
+		} else if dataCriteriaOid == "2.16.840.1.113883.3.560.1.72" {
+			if transferTo := &entry.(*Encounter).TransferTo; transferTo != nil {
+				transferTo.Codes[transferTo.CodeSystem] = []string{transferTo.Code}
+				if len(transferTo.Coded.CodesInCodeSet(codes[0].Set)) > 0 {
+					filteredEntries = append(filteredEntries, entry)
+				}
+			}
+		} else {
+			if entryData.IsInCodeSet(codes) && entryData.NegationInd != nil {
+				if *entryData.NegationInd == dataCriteria.Negation {
+					filteredEntries = append(filteredEntries, entry)
+				}
+			} else if entryData.IsInCodeSet(codes) && entryData.NegationInd == nil && !dataCriteria.Negation {
+				filteredEntries = append(filteredEntries, entry)
+			}
 		}
 	}
-
 	return filteredEntries
 }
 
@@ -248,6 +224,7 @@ func (r *Record) handlePatientExpired() []HasEntry {
 	return nil
 }
 
+// TODO: most likely belongs on a `type InsuranceProvidersGroup []InsuranceProviders`
 func (r *Record) handlePayerInformation() []HasEntry {
 	providers := make([]HasEntry, len(r.InsuranceProviders))
 	for _, prov := range r.InsuranceProviders {
@@ -256,6 +233,8 @@ func (r *Record) handlePayerInformation() []HasEntry {
 	return providers
 }
 
+
+// TODO: this belongs on CodeSet. 
 func reasonInCodes(code CodeSet, reason CodedConcept) bool {
 	for _, value := range code.Values {
 		if reason.Code == value.Code && reason.CodeSystem == value.CodeSystem {
@@ -263,4 +242,59 @@ func reasonInCodes(code CodeSet, reason CodedConcept) bool {
 		}
 	}
 	return false
+}
+
+// ResolveReference takes a Reference object, and finds the Entry that it refers to
+func (r *Record) ResolveReference(ref Reference) HasEntry {
+	switch ref.ReferencedType {
+		case "Conditions":
+			for _, entry := range r.Conditions {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+		case "Allergies":
+			for _, entry := range r.Allergies {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+		case "Medications":
+			for _, entry := range r.Medications {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+		case "Procedures":
+			for _, entry := range r.Procedures {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+		case "ProviderPerformances":
+			for _, entry := range r.ProviderPerformances {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+		case "InsuranceProviders":
+			for _, entry := range r.InsuranceProviders {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+		case "LabResults":
+			for _, entry := range r.LabResults {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+		case "Encounters":
+			for _, entry := range r.Encounters {
+				if entry.GetEntry().ID.Extension == ref.ReferencedID {
+					return &entry
+				}
+			}
+	}
+	return nil
 }
