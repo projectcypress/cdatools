ea7a80f9a01545e512d730b5a26712990de16978
Czulada, David S <dczulada@mitre.org>
2017-05-01 13:24:51 -0400

    times and names

--- models/medical_equipment.go
+++ models/medical_equipment.go
@@ -3,6 +3,6 @@ package models
 type MedicalEquipment struct {
 	Entry               `bson:",inline"`
 	Manufacturer        string       `json:"manufacturer,omitempty"`
-	AnatomicalStructure CodedConcept `json:"anatomical_structure,omitempty"`
+	AnatomicalStructure CodedConcept `json:"anatomical_target,omitempty"`
 	RemovalTime         *int64       `json:"removal_time,omitempty"`
 }
---
+++ models/vital_sign.go
@@ -0,0 +1,6 @@
+package models
+
+type VitalSign struct {
+	Entry            `bson:",inline"`
+}
+
