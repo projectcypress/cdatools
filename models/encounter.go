ed14ac64cab753ad3635e870bafe0bcf72a3b2eb
Andrew Hubley <ahubley@mitre.org>
2017-05-10 13:08:34 -0400

    fixed json binding for principal diagnosis, fixed index call to MapDataCriteria.FieldOids

--- models/encounter.go
+++ models/encounter.go
@@ -8,7 +8,7 @@ type Encounter struct {
 	TransferTo           Transfer          `json:"transferTo,omitempty"`
 	TransferFrom         Transfer          `json:"transferFrom,omitempty"`
 	Facility             Facility          `json:"facility,omitempty"`
-	PrincipalDiagnosis   Coded             `json:"principal_diagnosis,omitempty"`
+	PrincipalDiagnosis   Coded             `json:"principalDiagnosis,omitempty"`
 	Diagnosis            Coded             `json:"diagnosis,omitempty"`
 }
 
