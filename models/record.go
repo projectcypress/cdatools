d5d17203d97009228577fb4b3d8578e6a51268b1
Czulada, David S <dczulada@mitre.org>
2017-05-18 11:55:57 -0400

    Export payer

--- models/record.go
+++ models/record.go
@@ -2,7 +2,9 @@ package models
 
 import (
 	"regexp"
+	"github.com/pborman/uuid"
 )
+
 
 type Record struct {
 	Person
@@ -131,8 +133,15 @@ func (r *Record) GetEntriesForOids(dataCriteria DataCriteria, codes []CodeSet, o
 							transferFrom.Codes = make(map[string][]string)
 						}
 						transferFrom.Codes[transferFrom.CodeSystem] = []string{transferFrom.Code}
-						if len(codes) > 0 && len(transferFrom.Coded.CodesInCodeSet(codes[0].Set)) > 0 {
-							entries = append(entries, entry)
+						if (len(codes) > 0 && codeSetContainsCode(codes,transferFrom.CodedConcept)) {
+							newId := ObjectIdentifier{ID: uuid.New()}
+							newEntry := Entry{Oid: "2.16.840.1.113883.3.560.1.71", ObjectIdentifier: newId}
+							newEntry.StartTime = transferFrom.Time
+							newEntry.EndTime = transferFrom.Time
+							newTransCode := CodedConcept{Code: transferFrom.Code, CodeSystem: transferFrom.CodeSystem}
+							newTrans := Transfer{CodedConcept: newTransCode, Time: transferFrom.Time}
+							var newTransElm HasEntry = &Encounter{Entry: newEntry, TransferFrom: newTrans}
+							entries = append(entries, newTransElm)
 						}
 					}
 				} else if dataCriteriaOid == "2.16.840.1.113883.3.560.1.72" {
@@ -141,8 +150,15 @@ func (r *Record) GetEntriesForOids(dataCriteria DataCriteria, codes []CodeSet, o
 							transferTo.Codes = make(map[string][]string)
 						}
 						transferTo.Codes[transferTo.CodeSystem] = []string{transferTo.Code}
-						if len(codes) > 0 && len(transferTo.Coded.CodesInCodeSet(codes[0].Set)) > 0 {
-							entries = append(entries, entry)
+						if len(codes) > 0 && codeSetContainsCode(codes,transferTo.CodedConcept) {
+							newId := ObjectIdentifier{ID: uuid.New()}
+							newEntry := Entry{Oid: "2.16.840.1.113883.3.560.1.72", ObjectIdentifier: newId}
+							newEntry.StartTime = transferTo.Time
+							newEntry.EndTime = transferTo.Time
+							newTransCode := CodedConcept{Code: transferTo.Code, CodeSystem: transferTo.CodeSystem}
+							newTrans := Transfer{CodedConcept: newTransCode, Time: transferTo.Time}
+							var newTransElm HasEntry = &Encounter{Entry: newEntry, TransferTo: newTrans}
+							entries = append(entries, newTransElm)
 						}
 					}
 				} else {
@@ -182,17 +198,17 @@ func (r *Record) EntriesForDataCriteria(dataCriteria DataCriteria, vsMap map[str
 		case "2.16.840.1.113883.3.560.1.3", "2.16.840.1.113883.3.560.1.11":
 			entries = r.GetEntriesForOids(dataCriteria, codes, "2.16.840.1.113883.3.560.1.3", "2.16.840.1.113883.3.560.1.11")
 		case "2.16.840.1.113883.3.560.1.71", "2.16.840.1.113883.3.560.1.72":
-			// Transfers (either from or to)
+	//		// Transfers (either from or to)
 			if dataCriteria.FieldValues != nil {
-				if codeListID := dataCriteria.FieldValues["TRANSFER_FROM"].CodeListID; codeListID == "" {
-					if codeListID = dataCriteria.FieldValues["TRANSFER_TO"].CodeListID; codeListID != "" {
-						codes = vsMap[codeListID]
-					}
-				} else {
-					codes = vsMap[codeListID]
+				if dataCriteria.FieldValues["TRANSFER_FROM"].CodeListID != "" {
+					codes = vsMap[dataCriteria.FieldValues["TRANSFER_FROM"].CodeListID]
+					entries = r.GetEntriesForOids(dataCriteria, codes, dataCriteriaOid, "2.16.840.1.113883.3.560.1.79")
+				} else if dataCriteria.FieldValues["TRANSFER_TO"].CodeListID != "" {
+					codes = vsMap[dataCriteria.FieldValues["TRANSFER_TO"].CodeListID]
+					entries = r.GetEntriesForOids(dataCriteria, codes, dataCriteriaOid, "2.16.840.1.113883.3.560.1.79")
 				}
 			}
-			entries = r.GetEntriesForOids(dataCriteria, codes, dataCriteriaOid, "2.16.840.1.113883.3.560.1.79")
+
 		default:
 			entries = r.GetEntriesForOids(dataCriteria, codes, dataCriteriaOid)
 		}
@@ -309,8 +325,9 @@ func allDataCriteria(measures []Measure) []DataCriteria {
 
 // TODO: most likely belongs on a `type InsuranceProvidersGroup []InsuranceProviders`
 func (r *Record) handlePayerInformation() []HasEntry {
-	providers := make([]HasEntry, len(r.InsuranceProviders))
+	providers := make([]HasEntry, 0)
 	for _, prov := range r.InsuranceProviders {
+		prov.Oid = "2.16.840.1.113883.3.560.1.405"
 		providers = append(providers, &prov)
 	}
 	return providers
