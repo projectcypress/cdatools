73cea26be73a5947d406152ce44a9ca69cfcd53a
Czulada, David S <dczulada@mitre.org>
2017-06-13 11:44:14 -0400

    Support import of Assessment and Encounter Diagnosis and Principal Diagnosis

---
+++ importer/assessment.go
@@ -0,0 +1,24 @@
+package importer
+
+import (
+	"github.com/jbowtie/gokogiri/xml"
+	"github.com/jbowtie/gokogiri/xpath"
+	"github.com/projectcypress/cdatools/models"
+)
+
+func AssessmentPerformedExtractor(entry *models.Entry, entryElement xml.Node) interface{} {
+	assessmentPerformed := models.Assessment{}
+	assessmentPerformed.Entry = *entry
+	
+	var codePath = xpath.Compile("cda:code")
+	ExtractCodes(&assessmentPerformed.Entry.Coded, entryElement, codePath)
+	ExtractReasonOrNegation(&assessmentPerformed.Entry, entryElement)
+	scalarPath := xpath.Compile("cda:value")
+	ExtractValues(&assessmentPerformed.Entry, entryElement, scalarPath)
+
+	// set Status Code
+	assessmentPerformed.StatusCode = map[string][]string{}
+	assessmentPerformed.StatusCode["HL7 ActStatus"] = []string{"performed"}
+
+	return assessmentPerformed
+}
--- importer/procedure.go
+++ importer/procedure.go
@@ -10,10 +10,7 @@ func DiagnosticStudyOrderExtractor(entry *models.Entry, entryElement xml.Node) i
 	diagnosticStudyOrder := models.Procedure{}
 	diagnosticStudyOrder.Entry = *entry
 
-	// extract codes
-	var codePath = xpath.Compile("cda:code")
-	ExtractCodes(&diagnosticStudyOrder.Entry.Coded, entryElement, codePath)
-
+	extractBaseProcedure(&diagnosticStudyOrder, entryElement)
 	// extract order specific dates
 	var orderTimeXPath = xpath.Compile("cda:author/cda:time/@value")
 	diagnosticStudyOrder.StartTime = GetTimestamp(orderTimeXPath, entryElement)
